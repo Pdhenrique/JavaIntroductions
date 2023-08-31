89867048d4b3a55c66aabb4b424acb5c8d966b9c
Pedro Santana <pedroschw.c@gmail.com>
2023-08-31 12:33:39 -0300

    Testing repo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String movieName = "Harry-Potter";
+
+        double temperatura = 25;
+
+        double toFahrenheit = (temperatura * 1.8) + 32;
+
+        System.out.println(toFahrenheit);
+    }
+}
